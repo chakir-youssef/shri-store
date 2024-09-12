eda811b19baab5649d4e30e9a0f32957936fcc18
Youssef Chakir <youssef.chakir@e-polytechnique.ma>
2024-09-12 10:34:14 +0100

    solve responisve problem

--- src/app/components/shop-head/shop-head.component.ts
+++ src/app/components/shop-head/shop-head.component.ts
@@ -1,39 +1,39 @@
-import {Component} from '@angular/core';
-import {DataServiceService} from "../../services/data-service.service";
-import {DataStorageService} from "../../services/data-storage.service";
-
+import { Component } from '@angular/core';
+import { DataServiceService } from '../../services/data-service.service';
+import { DataStorageService } from '../../services/data-storage.service';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-shop-head',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './shop-head.component.html',
-  styleUrl: './shop-head.component.css'
+  styleUrl: './shop-head.component.css',
 })
 export class ShopHeadComponent {
-  constructor(private dataService: DataServiceService, private dataStorage: DataStorageService) {
-  }
+  isOpen: number = 0;
+  constructor(
+    private dataService: DataServiceService,
+    private dataStorage: DataStorageService
+  ) {}
 
   getAllProducts() {
-    this.dataService.getAllProducts().subscribe(
-      products => {
-        this.dataStorage.setProducts(products);
-      }
-    )
+    this.dataService.getAllProducts().subscribe((products) => {
+      this.dataStorage.setProducts(products);
+    });
+    this.isOpen = 0;
   }
 
   getMenProducts() {
-    this.dataService.getMenProducts().subscribe(
-      products => {
-        this.dataStorage.setProducts(products);
-      }
-    )
+    this.dataService.getMenProducts().subscribe((products) => {
+      this.dataStorage.setProducts(products);
+    });
+    this.isOpen = 1;
   }
 
   getWomenProducts() {
-    this.dataService.getWomenProducts().subscribe(
-      products => {
-        this.dataStorage.setProducts(products);
-      }
-    )
+    this.dataService.getWomenProducts().subscribe((products) => {
+      this.dataStorage.setProducts(products);
+    });
+    this.isOpen = 2;
   }
 }
